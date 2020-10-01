ac18e4a98394692b0745bc7c9fb7fa447b06b81c
Miguel A. Alvarez <miguelkashir@gmail.com>
2020-10-01 20:44:46 +0200

    refactor: reply to trigger message

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 const TelegramBot = require('node-telegram-bot-api');
+
 const { token } = require('./token');
+
 const bot = new TelegramBot(token, { polling: true });
+const REPLY = 'Eso dijo ella 😉';
+const TRIGGERS = ['que grande'];
 
-bot.on('message', (msg) => {
-  if (msg.text === 'que grande') {
-    bot.sendMessage(msg.chat.id, 'eso dijo ella');
-  }
+const formatReceivedText = text => {
+  return text.toLowerCase();
+};
+
+bot.on('message', msg => {
+  const formattedText = formatReceivedText(msg.text);
+  const match = TRIGGERS.find(trigger => trigger === formattedText);
+
+  if (match)
+    bot.sendMessage(msg.chat.id, REPLY, {
+      reply_to_message_id: msg.message_id
+    });
 });
 
-bot.on('polling_error', (error) => {
-  console.log(`Error: ${error.code}`);
-});
+bot.on('polling_error', error => console.error(`Error: ${error}`));
